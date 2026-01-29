0d1fa1b951b6879eae5fed6b89719afc7fdd9839
dion <dion.areglo1234@gmail.com>
2026-01-29 21:18:57 +0800

    f

--- src/components/LetterGlitch.jsx
+++ src/components/LetterGlitch.jsx
@@ -230,7 +230,7 @@ const LetterGlitch = ({
 
 
   return (
-    <div className="relative w-full h-full bg-black overflow-hidden">
+    <div className="relative w-full h-screen bg-black overflow-hidden">
       <canvas ref={canvasRef} className="block w-full h-screen" />
       {outerVignette && (
         <div className="absolute top-0 left-0 w-full h-full pointer-events-none bg-[radial-gradient(circle,_rgba(0,0,0,0)_60%,_rgba(0,0,0,1)_100%)]"></div>
